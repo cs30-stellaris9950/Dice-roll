49024fe831cbefd842e0ab182de1736690934ce6
Andy Wei <a.wei4@EDU.epsb.ca>
2023-09-08 12:59:16 -0600

    setup

---
+++ main.py
@@ -0,0 +1,16 @@
+# Function Name: roll2Dice
+# Purpose: Use random numbers to simulate the results of rolling 2 six-sided dice.
+# Output the result of the roll by printing the number on each die as well as the sum of the two dice.
+#
+# eg. Rolling a 5 and a 2 should be outputted as: “5,2 (sum: 7)”
+#
+# Also return the sum of the two dice.
+# Parameters:
+# No Parameters
+# Return Value:
+# The sum of rolling the two dice.
+#
+
+
+
+def roll2Dice():
